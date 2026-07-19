3b93022decae9a8d18adfa47cdf504771ccdb096
Inn <dulsfnj413@naver.com>
2026-07-20 07:07:58 +0900

    notBlank 추가

--- src/main/java/com/example/movie_crud/review/dto/CreateReviewRequest.java
+++ src/main/java/com/example/movie_crud/review/dto/CreateReviewRequest.java
@@ -1,10 +1,12 @@
 package com.example.movie_crud.review.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class CreateReviewRequest {
 
     private String content;
+    @NotBlank
     private Double rating;
 }
--- src/main/java/com/example/movie_crud/review/dto/UpdateReviewRequest.java
+++ src/main/java/com/example/movie_crud/review/dto/UpdateReviewRequest.java
@@ -1,10 +1,12 @@
 package com.example.movie_crud.review.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class UpdateReviewRequest {
 
     private String content;
+    @NotBlank
     private Double rating;
 }
