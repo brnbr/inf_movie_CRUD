c8ac9773159931459685e184661bbbb9b5a28257
Inn <dulsfnj413@naver.com>
2026-07-20 07:06:47 +0900

    notBlank 추가

--- src/main/java/com/example/movie_crud/movie/dto/CreateMovieReqeust.java
+++ src/main/java/com/example/movie_crud/movie/dto/CreateMovieReqeust.java
@@ -1,10 +1,12 @@
 package com.example.movie_crud.movie.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class CreateMovieReqeust {
 
+    @NotBlank
     private String title;
     private String description;
 }
