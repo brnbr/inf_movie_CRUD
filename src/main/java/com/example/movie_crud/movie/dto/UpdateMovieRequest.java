e7fb3291ff58e4339811ea6bb63bdb28d7d2e7b6
Inn <dulsfnj413@naver.com>
2026-07-20 07:07:10 +0900

    notBlank 추가

--- src/main/java/com/example/movie_crud/movie/dto/UpdateMovieRequest.java
+++ src/main/java/com/example/movie_crud/movie/dto/UpdateMovieRequest.java
@@ -1,10 +1,12 @@
 package com.example.movie_crud.movie.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
 public class UpdateMovieRequest {
 
+    @NotBlank
     private String title;
     private String description;
 }
