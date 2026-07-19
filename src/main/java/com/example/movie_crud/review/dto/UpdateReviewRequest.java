b14fda35c9bf663fb1eabd492a195de4d20a8b1b
Inn <dulsfnj413@naver.com>
2026-07-20 07:10:10 +0900

    notblank -> size/min 변경

--- src/main/java/com/example/movie_crud/review/dto/UpdateReviewRequest.java
+++ src/main/java/com/example/movie_crud/review/dto/UpdateReviewRequest.java
@@ -1,12 +1,15 @@
 package com.example.movie_crud.review.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class UpdateReviewRequest {
 
+    @Size(min = 5, max = 200)
     private String content;
-    @NotBlank
+    @Min(0)
     private Double rating;
 }
